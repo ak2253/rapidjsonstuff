542b81f89d87df1f6539b1ff502922ccf62ae31b
ak2253 <61290235+ak2253@users.noreply.github.com>
2020-05-09 18:46:09 -0400

    Update testcase.cpp

--- Checker/testcase.cpp
+++ Checker/testcase.cpp
@@ -5,13 +5,14 @@
 using namespace std;
 
 int main() {
-    //BFV
-    //NULL
+    //fail
     string str2="unsigned int m, const PlaintextModulus ptModulus";
+    //fail
     string str3="shared_ptr<typename Element::Params> params, const PlaintextModulus &plaintextModulus, float distributionParameter, float assuranceMeasure, float securityLevel, usint relinWindow, float distributionParmStst, int depth = 1";
-    //TODO:: check order with other groups
-	string str4="shared_ptr<typename Element::Params> params, const PlaintextModulus plaintextmodulus, usint relinWindow, float stDev, float stDevStSt, int depth = 1, int assuranceMeasure = 9, float securityLevel = 1.006";
-	string path1="stst.json";
+	//pass
+    string str4="shared_ptr<typename Element::Params> params, const PlaintextModulus plaintextmodulus, usint relinWindow, float stDev, float stDevStSt, int depth = 1, int assuranceMeasure = 9, float securityLevel = 1.006";
+	//json path names for reference
+    string path1="stst.json";
     string path2="Validate.json";
     if(check(str4)) {
         cout<<"Nice!"<<endl;
