ad7f902dccc65a3208f6d09647ee73969dd7ad88
ak2253 <61290235+ak2253@users.noreply.github.com>
2020-05-13 20:33:11 -0400

    Update testcase.cpp

--- Checker/testcase.cpp
+++ Checker/testcase.cpp
@@ -8,12 +8,18 @@ int main() {
     //fail
     string str2="unsigned int m, const PlaintextModulus ptModulus";
     //fail
-    string str3="shared_ptr<typename Element::Params> params, const PlaintextModulus &plaintextModulus, float distributionParameter, float assuranceMeasure, float securityLevel, usint relinWindow, float distributionParmStst, int depth = 1";
+    string awful="shared_ptr<typename Element::Params> params, const FakeModulus badplaintext, float distributionParameter, float assuranceMeasure, float securityLevel, usint relinWindow, float distributionParmStst, int depth = 1";
 	//pass
     string str4="shared_ptr<typename Element::Params> params, const PlaintextModulus plaintextmodulus, usint relinWindow, float stDev, float stDevStSt, int depth = 1, int assuranceMeasure = 9, float securityLevel = 1.006";
 	//json path names for reference
     string path1="stst.json";
     string path2="Validate.json";
+    if(check(awful)) {
+        cout<<"Nice!"<<endl;
+    }
+    else {
+        cout<<"Oh No!"<<endl;
+    }
     if(check(str4)) {
         cout<<"Nice!"<<endl;
     }
